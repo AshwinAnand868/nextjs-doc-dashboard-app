4879e5e7e8b11727bf1919b6ff080fe3a59a6ed8
AshwinAnand868 <anandashwin868@gmail.com>
2024-03-15 12:47:52 -0400

    Chapter 3 finished - optimising fonts and images using next

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import {lusitana} from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -22,11 +23,25 @@ export default function Page() {
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Log in</span> 
+            <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className='hidden md:block'
+            alt='Dashboard project screenshot for desktop devices'
+          />
+          <Image 
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className='block md:hidden'
+            alt='Dashboard project screenshot for mobile devices'
+          />
         </div>
       </div>
     </main>
