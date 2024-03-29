8b8d116ec4b34a3bb577a8608c72340234a1fcf9
AshwinAnand868 <anandashwin868@gmail.com>
2024-03-29 01:32:57 -0400

    Finish Chapter 12 - Mutating Data using Server actions

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,38 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/edit-form";
+
+
+export default async function Page({params}: {
+    params: {
+        id: string
+    }
+}) {
+
+    const id = params.id;
+    // const customers = await fetchCustomers();
+    // const invoice = await fetchInvoiceById(id);
+
+    // fetching date in parallel
+    const [customers, invoice] = await Promise.all([
+        fetchCustomers(),
+        fetchInvoiceById(id)
+    ]);
+
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true
+                    }
+                ]}
+            />
+
+            <Form invoice={invoice} customers={customers} />
+        </main>
+    )
+}
